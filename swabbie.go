5570b77bec5c73b744d9b73839bbd75136c39294
Joseph Kern <jkern@semafour.net>
2011-07-01 17:02:33 -0400

    Removed the indesPage function.

--- swabbie.go
+++ swabbie.go
@@ -13,17 +13,12 @@ type Page struct {
 	Body  []byte
 }
 
-func indexPage(w http.ResponseWriter, r *http.Request) (title string, err os.Error) {
-	title = r.URL.Path[1:]
-	if title != "" {
-		return
-	}
-	title = "index.html"
-	return
-}
-
 // Load a file from the OS, return a Page struct or error
-func loadPage(title string)(*Page, os.Error) {
+func loadPage(w http.ResponseWriter, r *http.Request)(*Page, os.Error) {
+	title := r.URL.Path[1:]
+	if title == "" {
+		title = "index.html"
+	}
 	body, err := ioutil.ReadFile(title)
 	if err != nil {
 		return nil, err
@@ -32,11 +27,7 @@ func loadPage(title string)(*Page, os.Error) {
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := indexPage(w, r)
-	if err != nil {
-		return
-	}
-	p, err := loadPage(title)
+	p, err := loadPage(w, r)
 	if err != nil {
 		http.NotFound(w, r)
 		return
